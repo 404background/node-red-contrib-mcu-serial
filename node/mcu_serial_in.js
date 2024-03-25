e7cabc582d9459b4e31c1a8fa116a3795e781f3f
404background <404background@gmail.com>
2024-03-25 15:25:37 +0900

    Fix programs (mcu_serial_in)

--- node/mcu_serial_in.js
+++ node/mcu_serial_in.js
@@ -1,15 +1,7 @@
-const Serial_in = require('./serial_in')
-
 module.exports = function(RED) {
     function Serial_in(config) {
         RED.nodes.createNode(this, config);
 		console.log(config)
-
-        const serial = new Serial_in()
-        serial.onReadable = function() {
-            const data = serial.read()
-            node.send({payload: data})
-        }
     }
     RED.nodes.registerType("mcu_serial_in", Serial_in);
 }
