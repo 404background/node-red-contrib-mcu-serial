a0c6ed8926b98357a6399f8e9251d85f51950a5a
404background <404background@gmail.com>
2024-03-26 11:34:50 +0900

    Fix programs (to mcu_serial)

--- node/mcu_serial.js
+++ node/mcu_serial.js
@@ -1,15 +1,13 @@
 module.exports = function(RED) {
     function Serial_in(config) {
-        RED.nodes.createNode(this, config);
+        RED.nodes.createNode(this, config)
 		console.log(config)
     }
-    RED.nodes.registerType("mcu_serial_in", Serial_in);
-}
+    RED.nodes.registerType("mcu_serial_in", Serial_in)
 
-module.exports = function(RED) {
     function Serial_out(config) {
-        RED.nodes.createNode(this, config);
+        RED.nodes.createNode(this, config)
 		console.log(config)
     }
-    RED.nodes.registerType("mcu_serial_out", Serial_out);
+    RED.nodes.registerType("mcu_serial_out", Serial_out)
 }
